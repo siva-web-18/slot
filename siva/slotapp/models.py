493094e0ff92b891364187d14765fb717da1e947
siva-web-18 <sivarajendran6700@gmail.com>
2025-11-28 10:48:21 +0530

    Sucess

--- siva/slotapp/models.py
+++ siva/slotapp/models.py
@@ -1,32 +1,3 @@
-# Ex02 Time Table
-## Date:
-
-## AIM
-To write a html webpage page to display your slot timetable.
-
-## ALGORITHM
-### STEP 1
-Create a Django-admin Interface.
-
-### STEP 2
-Create an App inside the Django project.
-
-### STEP 2
-Create a static folder uder the created App and insert HTML code.
-
-### STEP 3
-Create a simple table using ```<table>``` tag in html with the relevant attributes.
-
-### STEP 4
-Add rows using ```<tr>``` tag.
-
-### STEP 5
-Add your course schedule using ```<td>``` tag.
-
-### STEP 6
-Execute the program using runserver command.
-
-## PROGRAM
 <html>
     <head>
         <title>
@@ -36,7 +7,7 @@ Execute the program using runserver command.
     <body>
         <img src="logo.png" width="1500" height="300">
         <table border="1" cellspacing="5" cellpadding="5">
-            <caption>SLOT TIMETABLE-Siva R(25007668)</caption>
+            <caption>SLOT TIMETABLE-S.R.NIVEDHITHA(25000724)</caption>
             <tr bgcolor="cyan">
                 <th>Day/Time</th>
                 <th>Monday</th>
@@ -112,11 +83,3 @@ Execute the program using runserver command.
         </table>
     </body>
 </html>
-
-## OUTPUT
-```
-c:\Users\acer\Pictures\Screenshots\Screenshot 2025-11-26 152028.png
-```
-
-## RESULT
-The program for creating slot timetable using basic HTML tags is executed successfully.
